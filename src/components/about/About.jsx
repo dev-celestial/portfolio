5895da063fe0380f47425f5eb40dae48bafa597d
SATYAM KUMAR <workmail.celestial@gmail.com>
2024-08-10 12:13:04 +0530

    Cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css";
 import AboutImg from "../../assets/profile3.jpg"
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/Satyam-Kumar.pdf";
 import Info from './Info';
 
 const About = () => {
